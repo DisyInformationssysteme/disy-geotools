eb016edde18eff6677a6ee77091c16eca798e7da
Various Authors <info@disy.net>
2021-02-09 17:20:23 +0100

    Version 0.5.1: Update to GeoTools 23.4 and adjust path to CoordinateSequenceUtilitiesAdapter

--- src/main/java/net/disy/gis/org/geotools/gml/util/DelegatingCoordinateSequenceUtilities.java
+++ src/main/java/net/disy/gis/org/geotools/gml/util/DelegatingCoordinateSequenceUtilities.java
@@ -3,24 +3,20 @@ package net.disy.gis.org.geotools.gml.util;
 
 import org.locationtech.jts.geom.CoordinateSequence;
 
-// NOT_PUBLISHED
-public class DelegatingCoordinateSequenceUtilities {
+public final class DelegatingCoordinateSequenceUtilities {
 
   private DelegatingCoordinateSequenceUtilities() {}
 
-  private static final String DEFAULT_IMPLEMENTATION = "de.disy.gis.gml.org.geotools.adapter.CoordinateSequenceUtilitiesAdapter";
+  private static final String DEFAULT_IMPLEMENTATION = "net.disy.gis.geotools.gml.adapter.CoordinateSequenceUtilitiesAdapter";
 
-  private static ICoordinateSequenceUtilities coordinateSequenceUtilities;
+  private static final ICoordinateSequenceUtilities coordinateSequenceUtilities;
 
   static {
     try {
       final Class<?> clazz = Class.forName(DEFAULT_IMPLEMENTATION);
       coordinateSequenceUtilities = (ICoordinateSequenceUtilities) clazz.getDeclaredConstructor().newInstance();
-    }
-    catch (Exception e) {
-      throw new RuntimeException(
-          "Can't create default implementation  " + DEFAULT_IMPLEMENTATION,
-          e);
+    } catch (Exception e) {
+      throw new RuntimeException("Can't create default implementation  " + DEFAULT_IMPLEMENTATION, e);
     }
   }
 
